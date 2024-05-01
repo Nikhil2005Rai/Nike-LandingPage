730caaf853fff1c1426114469fc8f061fbc49c01
Nikhil2005Rai <n2005rai@gmail.com>
2024-05-01 20:59:35 +0530

    start

---
+++ src/Navigation.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <img src="../assets/brand_logo.png" alt="logo" />
+      </div>
+      <ul>
+        <li>MENU</li>
+        <li>LOCATION</li>
+        <li>ABOUT</li>
+        <li>CONTACT</li>
+      </ul>
+      <button>Login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
